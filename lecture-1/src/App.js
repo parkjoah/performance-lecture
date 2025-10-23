56f03b70c253463033fe01c4fe2f6bd4a8fda966
조아 <parkjoah@naver.com>
2025-10-24 02:30:14 +0900

    lecture1 : bundle & code-splitting

--- lecture-1/src/App.js
+++ lecture-1/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import ListPage from './pages/ListPage/index';
-import ViewPage from './pages/ViewPage/index';
+import React, { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+const ListPage = lazy(() => import(`./pages/ListPage/index`));
+const ViewPage = lazy(() => import(`./pages/ViewPage/index`));
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<ListPage />} />
-        <Route path="/view/:id" element={<ViewPage />} />
-      </Routes>
+      <Suspense fallback={<div>로딩중...</div>}>
+        <Routes>
+          <Route path="/" Component={ListPage} exact />
+          <Route path="/view/:id" Component={ViewPage} exact />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
